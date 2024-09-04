4ecb7a5894d015bb63f292865f805eb64e219798
Mark Barnes <barnemar002@warren.k12.in.us>
2024-09-04 13:42:10 -0400

    initial

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+    }
+
+
+    public static boolean isPalindrome(int number) {
+        number = Math.abs(number);
+
+        int numberClone = number;
+        int reverse = 0;
+
+
+        int lastDigit = number % 10;
+        reverse *= 10;
+        int sum = reverse + lastDigit;
+
+        while (number > 0) {
+            lastDigit = number % 10;
+            reverse *= 10;
+            reverse += lastDigit;
+            number /= 10;
+        }
+
+
+        return reverse == numberClone;
+
+
+    }
+
+}
